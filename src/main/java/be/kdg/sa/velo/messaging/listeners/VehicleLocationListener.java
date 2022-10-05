aa4cfb0d0c6939314bca10d491ee112514616858
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2022-10-05 16:56:19 +0200

    Did vehiclelocation message processing

--- src/main/java/be/kdg/sa/velo/messaging/listeners/VehicleLocationListener.java
+++ src/main/java/be/kdg/sa/velo/messaging/listeners/VehicleLocationListener.java
@@ -1,5 +1,11 @@
 package be.kdg.sa.velo.messaging.listeners;
 
+import be.kdg.sa.velo.models.vehicles.messages.VehicleLocationPingMessage;
+import be.kdg.sa.velo.services.VehicleService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +15,31 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class VehicleLocationListener {
+	
+	private final VehicleService vehicleService;
+	private final ObjectMapper objectMapper;
+	private final Logger logger = LoggerFactory.getLogger (VehicleLocationListener.class);
+	
+	public VehicleLocationListener (VehicleService vehicleService, ObjectMapper objectMapper) {
+		this.vehicleService = vehicleService;
+		this.objectMapper = objectMapper;
+	}
+	
+	private void receiveVehicleLocation (VehicleLocationPingMessage vehicleLocation) {
+		vehicleService.vehicleLocationPing (vehicleLocation);
+	}
+	
 	@RabbitListener (queues = "${messaging.routingKey}")
 	void receive (String message) {
-		System.out.println ("Received: " + message);
+		try {
+			var vehicleLocation = deserializeVehicleLocation (message);
+			receiveVehicleLocation (vehicleLocation);
+		} catch (JsonProcessingException e) {
+			logger.error ("Could not parse " + message, e);
+		}
+	}
+	
+	private VehicleLocationPingMessage deserializeVehicleLocation (String message) throws JsonProcessingException {
+		return objectMapper.readValue (message, VehicleLocationPingMessage.class);
 	}
 }
