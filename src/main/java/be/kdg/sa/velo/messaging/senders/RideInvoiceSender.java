abe3ec8791e21844cbb4893462db8a180b152f27
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2022-10-05 11:03:56 +0200

    Added RideInvoiceSender as Component

--- src/main/java/be/kdg/sa/velo/messaging/senders/RideInvoiceSender.java
+++ src/main/java/be/kdg/sa/velo/messaging/senders/RideInvoiceSender.java
@@ -1,8 +1,11 @@
 package be.kdg.sa.velo.messaging.senders;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Jonas Leijzen
  * 5/10/2022
  */
+@Component
 public class RideInvoiceSender {
 }
