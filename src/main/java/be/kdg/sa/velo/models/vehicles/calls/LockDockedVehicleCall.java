9a028bf7302241434328084428e8136bf23df212
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2022-10-08 22:44:11 +0200

    Removed stationid from LockDockedVehicleCall because the station can be found via the lockid

--- src/main/java/be/kdg/sa/velo/models/vehicles/calls/LockDockedVehicleCall.java
+++ src/main/java/be/kdg/sa/velo/models/vehicles/calls/LockDockedVehicleCall.java
@@ -5,18 +5,11 @@ package be.kdg.sa.velo.models.vehicles.calls;
  * 21/09/2022
  */
 public final class LockDockedVehicleCall extends LockVehicleCall {
-	
-	private final int stationId;
 	private final int lockId;
 	
-	public LockDockedVehicleCall (int vehicleId, int userId, boolean defect, int stationId, int lockId) {
+	public LockDockedVehicleCall (int vehicleId, int userId, boolean defect, int lockId) {
 		super (vehicleId, userId, defect);
-		this.stationId = stationId;
 		this.lockId = lockId;
-	}
-	
-	public int getStationId () {
-		return stationId;
 	}
 	
 	public int getLockId () {
