2cd2f956af5eefa909b8108033fc2b493b63518d
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2022-10-23 12:18:24 +0200

    Added 2/3 methods of maintenance detection

---
+++ src/main/java/be/kdg/sa/velo/maintenance/qualifiers/TimeMaintenanceQualifier.java
@@ -0,0 +1,36 @@
+package be.kdg.sa.velo.maintenance.qualifiers;
+
+import be.kdg.sa.velo.exceptions.VehicleNotFoundException;
+import be.kdg.sa.velo.repositories.VehicleRepository;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+/**
+ * Jonas Leijzen
+ * 23/10/2022
+ */
+@Component
+public class TimeMaintenanceQualifier implements MaintenanceQualifier {
+	
+	private final VehicleRepository vehicleRepository;
+	
+	public TimeMaintenanceQualifier (VehicleRepository vehicleRepository) {
+		this.vehicleRepository = vehicleRepository;
+	}
+	
+	private Duration getDurationFromLastMaintenance (MaintenanceQualifyContext context) {
+		return Duration.between (vehicleRepository.findById (context.getVehicleId ())
+						.orElseThrow (() -> new VehicleNotFoundException (context.getVehicleId ())).getLastMaintenanceDate (),
+				LocalDateTime.now ());
+	}
+	
+	@Override
+	public boolean isMaintenanceNeeded (MaintenanceQualifyContext context) {
+	
+		int days = context.getVehicleTypeEnum ().isDocked () ? 8 * 7 : 3 * 7; // Eight or three weeks
+		return getDurationFromLastMaintenance (context).toDays () > days;
+	
+	}
+}
