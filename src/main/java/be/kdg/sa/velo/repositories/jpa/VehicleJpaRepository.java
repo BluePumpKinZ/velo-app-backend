c893fff4530b72c49c7ad4ce9aa10cbea4f00870
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2022-11-02 21:15:25 +0100

    Fixed database query for getting closest vehicle

--- src/main/java/be/kdg/sa/velo/repositories/jpa/VehicleJpaRepository.java
+++ src/main/java/be/kdg/sa/velo/repositories/jpa/VehicleJpaRepository.java
@@ -10,14 +10,19 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface VehicleJpaRepository extends JpaRepository<Vehicle, Integer> {
 	
-	@Query (value = """
-			SELECT TOP(1) * FROM VEHICLES
-			ORDER BY POINT.STDistance(CONCAT('POINT(', CONCAT(?1, CONCAT(' ', CONCAT(?2, ')')))))
-			""", nativeQuery = true)
+	String findClosestVehicleQuery = """
+			SELECT TOP(1) V.VEHICLEID, V.SERIALNUMBER, V.BIKELOTID, V.LASTMAINTENANCEON, V.LOCKID, V.POINT FROM VEHICLES V
+			JOIN BIKELOTS BL ON BL.BIKELOTID = V.BIKELOTID
+			WHERE BL.BIKETYPEID IN (3, 4)
+			ORDER BY Geography\\:\\:STGeomFromText(V.Point.MakeValid().STAsText(),4326)
+			.STDistance(Geography\\:\\:STGeomFromText(Geometry\\:\\:Point(?1,?2,4326).MakeValid().STAsText(),4326))
+			""";
+	
+	@Query (value = findClosestVehicleQuery, nativeQuery = true)
 	Vehicle findClosestVehicle (double latitude, double longitude);
 	
-	@Query(value = "SELECT BK.BIKETYPEDESCRIPTION FROM BikeTypes BK\n" +
-			"JOIN Bikelots BL ON BK.BIKETYPEID = BL.BIKETYPEID\n" +
+	@Query (value = "SELECT BK.BIKETYPEDESCRIPTION FROM BIKETYPES BK\n" +
+			"JOIN BIKELOTS BL ON BK.BIKETYPEID = BL.BIKETYPEID\n" +
 			"JOIN VEHICLES V ON BL.BIKELOTID = V.BIKELOTID\n" +
 			"WHERE V.VEHICLEID = ?1", nativeQuery = true)
 	VehicleType getVehicleType (int vehicleId);
